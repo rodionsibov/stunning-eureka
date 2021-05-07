35b202c2ba0685d25ff9801c3e778c0802c325aa
Rodion Sibov <rodionsibov@gmail.com>
2021-05-07 10:35:42 +0300

    first commit

---
+++ db/generate.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+    const faker = require('faker')
+    const _ = require('lodash')
+
+    return {
+        people: _.times(100, function (n) {
+            return {
+                id: n + 1,
+                name: faker.name.findName(),
+                avatar: faker.image.avatar(),
+                phone: faker.phone.phoneNumber(),
+                email: faker.internet.email(),
+                department: _.sample(['Customer Support', 'Engineering', 'Sales'])
+            }
+        })
+    }
+}
---
+++ src/components/SimpleList.vue
@@ -0,0 +1,63 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Department</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="person in people" :key="person.id">
+        <td>{{ person.name }}</td>
+        <td>{{ person.email }}</td>
+        <td>{{ person.phone }}</td>
+        <td>{{ person.department }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { onBeforeMount, ref } from "vue";
+
+export default {
+  setup() {
+    const people = ref([]);
+
+    onBeforeMount(() => {
+      fetch("http://localhost:3000/people")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          people.value = data;
+        });
+    });
+
+    return {
+      people,
+    };
+  },
+};
+</script>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
