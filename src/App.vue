0e7787e13aa44e0046f24a98bbf4a2e243f990fc
Rodion Sibov <rodionsibov@gmail.com>
2021-05-07 11:03:42 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+  <div class="mx-auto p-6 bg-gray-200">
+    <card-list />
+  </div>
   <simple-list />
 </template>
 
 <script>
 import SimpleList from "@/components/SimpleList.vue";
+import CardList from "@/components/CardList.vue";
 
 export default {
   name: "App",
   components: {
     SimpleList,
+    CardList,
   },
 };
 </script>
